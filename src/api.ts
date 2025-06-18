edc1fd7a860a9cf18d68dbc250cc7025617d0888
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2025-06-18 13:23:03 +0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_URL,
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 export default api;
